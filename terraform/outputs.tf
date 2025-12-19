5ad833663ae2879d4e742619b4cac95e3a8dd922
Seb Portable <sebastien.giguet@ynov.com>
2025-12-19 14:44:50 +0100

    test-pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "assign_public_ip" {
   description = "L'adresse IP publique de l'instance VM"
   value       = module.vm_module.public_ip
 }
+
+output "vm_public_ip" {
+  description = "Compatibilité CI: IP publique de la VM"
+  value       = module.vm_module.public_ip
+}
