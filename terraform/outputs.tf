26c5be0c1562efd487f0617be826055f82728d87
Seb Portable <sebastien.giguet@ynov.com>
2025-12-19 14:00:48 +0100

    test-pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "vm_public_ip" {
+output "assign_public_ip" {
   description = "L'adresse IP publique de l'instance VM"
-  value       = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+  value       = var.vm_ip
 }
